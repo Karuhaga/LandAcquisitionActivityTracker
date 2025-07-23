672b16b0bb687ca0f726312d26e3d7322d5d5539
Karuhaga <kardel111@gmail.com>
2025-07-23 18:49:17 +0300

    20250723

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 
 DATABASE_CONFIG = {
-    'SERVER': 'DELTA-PC\\SQLEXPRESS',  # Replace with your actual SQL Server instance
-    'DATABASE': 'BankReconciliationTracker',
-    'USERNAME': 'brt1',
+    'SERVER': 'DELTA-PC\\SQLEXPRESS',
+    'DATABASE': 'LandAcquisitionActivityTracker',
+    'USERNAME': 'laat',
     'PASSWORD': 'BUdaka123$%',
 }
